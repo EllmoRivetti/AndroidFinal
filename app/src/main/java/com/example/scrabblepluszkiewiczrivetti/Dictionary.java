7b99499aa8bb1a951ce442d7f6e900cb7d409925
PLUSZKIS <PLUSZKIS@pc5301v.lan.esiee.fr>
2019-12-13 10:11:26 +0100

    fin thread

--- app/src/main/java/com/example/scrabblepluszkiewiczrivetti/Dictionary.java
+++ app/src/main/java/com/example/scrabblepluszkiewiczrivetti/Dictionary.java
@@ -1,59 +1,86 @@
 package com.example.scrabblepluszkiewiczrivetti;
 
+import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
 import java.io.BufferedReader;
 import java.io.Console;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
-public class Dictionary {
+public class Dictionary extends AsyncTask<String, String, String> {
 
     private String[] wordList;
+    private MainActivity main;
 
-    public Dictionary(final String filename) throws FileNotFoundException
-    {
-        Thread t = new Thread(new Runnable(){
 
-            @Override
-            public void run() {
-                try
-                {
-                    File file = new File(filename);
+    // Runs in UI before background thread is called
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
 
-                    BufferedReader br = new BufferedReader(new FileReader(file));
-                    String line;
-                    while ((line = br.readLine()) != null)
-                    {
-                        /*if(line.matches("-?\\d+(\\.\\d+)?"))
-                        {
-                            this.wordList
-                        }
-                        else
-                        {
+        // Do something like display a progress bar
+    }
 
-                        }
-                        wordList*/
-                    }
-                    br.close();
+    @Override
+    protected String doInBackground(String... voids) {
+
+        try {
+            Log.i("Dict", "Starting file parse");
+            InputStream ips = main.getApplicationContext().getResources().openRawResource(R.raw.fr);
+            InputStreamReader ipsr = new InputStreamReader(ips);
+            BufferedReader br = new BufferedReader(ipsr);
+            String line;
+            int i = 0;
+            while ((line = br.readLine()) != null) {
+                if (i == 0) {
+                    int size = Integer.parseInt(line);
+                    wordList = new String[size];
+                } else {
+                    wordList[i - 1] = line;
+                    Log.i("Dict", wordList[i - 1]);
                 }
-                catch(IOException e)
-                {
-                    //Something wrong happened
-                }
-
-
+                i++;
             }
-        });
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        t.start();
+        return null;
+    }
+
+    // This is called from background thread but runs in UI
+    @Override
+    protected void onProgressUpdate(String... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPostExecute(final String unused) {
+        //Process the result here
+        super.onPostExecute(unused);
+        Log.i("Dict", "Done processing");
+
+        main.resume();
+    }
+
+    public Dictionary(MainActivity main)
+    {
+        this.main = main;
     }
 
     public boolean isValidWord(String word)
     {
-
-        return false;
+        Arrays.sort(wordList);
+        return Arrays.binarySearch(wordList, word) >= 0;
     }
-
 
 }
--- app/src/main/java/com/example/scrabblepluszkiewiczrivetti/MainActivity.java
+++ app/src/main/java/com/example/scrabblepluszkiewiczrivetti/MainActivity.java
@@ -5,42 +5,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
+
+import java.io.FileNotFoundException;
 
 public class MainActivity extends AppCompatActivity {
 
 
     public final static int REQUEST_CODE_ASK_PERMISSIONS = 1;
+    public Dictionary dict;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(checkSelfPermission(Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED)
-        {
-            requestPermissions(new String[] {Manifest.permission.READ_SMS}, MainActivity.REQUEST_CODE_ASK_PERMISSIONS);
+        if (checkSelfPermission(Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_SMS}, MainActivity.REQUEST_CODE_ASK_PERMISSIONS);
             requestPermissionReadSms();
         }
 
 
-
     }
 
-    public void requestPermissionReadSms()
+    public void resume()
     {
-            PermissionReadSmsDialog dialog = new PermissionReadSmsDialog();
-            dialog.show(getSupportFragmentManager(), "Dialog Permission Read SMS");
+
+        Boolean b = dict.isValidWord("évoquent");
+        Log.i("Dict", Boolean.toString(b));
+    }
+
+    public void requestPermissionReadSms() {
+        PermissionReadSmsDialog dialog = new PermissionReadSmsDialog();
+        dialog.show(getSupportFragmentManager(), "Dialog Permission Read SMS");
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
-    {
-        switch(requestCode)
-        {
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
             case REQUEST_CODE_ASK_PERMISSIONS:
-                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     Toast.makeText(this, "Permission acceptée !", Toast.LENGTH_LONG).show();
+                    dict = new Dictionary(this);
+                    dict.execute("");
                 }
                 break;
             default:
